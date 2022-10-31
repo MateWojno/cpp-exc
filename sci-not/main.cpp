785117a9d58dbf8a4ff131b0f6e8ba1b6724361e
MateWojno <mateusz.k.wojno@gmail.com>
2022-10-31 19:32:09 +0100

    Scientific notation float and double;

---
+++ sci-not/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float f1 = 35e3;
+    double d1 = 12E4;
+    cout << f1;
+    cout << "\n\n";
+    cout << d1;
+    cout << "\n\n";
+    return 0;
+}
